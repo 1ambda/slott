6d78ba4886885543693d91eaf01f9d29b749f674
1ambda <1amb4a@gmail.com>
2016-03-30 23:36:49 +0900

    fix: Remove env CONTAINER_ADDRESS

--- tools/url.js
+++ tools/url.js
@@ -1,12 +1,10 @@
 import { ENV_DEV, } from './env'
 
-const envAddress = process.env.CONTAINER_ADDRESS
 const DEVELOPMENT_CONTAINER_ADDRESS = 'http://localhost:3002' /** json mock-server, see package.json */
 const PRODUCTION_CONTAINER_ADDRESS = 'http://localhost:8080'
 
-let containerAddress = (process.env.NODE_ENV === ENV_DEV) ? DEVELOPMENT_CONTAINER_ADDRESS :
-  (envAddress === void 0) ? PRODUCTION_CONTAINER_ADDRESS :
-    envAddress /** used with `npm run start:client-cors` */
+let containerAddress = (process.env.NODE_ENV === ENV_DEV) ?
+  DEVELOPMENT_CONTAINER_ADDRESS : PRODUCTION_CONTAINER_ADDRESS
 
 export const CONTAINER_ADDRESS = containerAddress
 
